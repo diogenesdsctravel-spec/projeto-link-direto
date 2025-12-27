755029080a56c2f22ee81235150ebf09ef844eb2
Diógenes <dscardoso2@gmail.com>
2025-12-27 10:00:12 -0300

    fix: change router basename for Netlify

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/projeto-link-direto">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
